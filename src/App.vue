482d780e2a5b71126281a0ca585ec12f636defde
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-12-07 03:06:41 +0100

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <signUpForm/>
+  </div>
+</template>
+
+<script>
+import signUpForm from "./components/signUpForm.vue";
+
+
+export default {
+  name: "App",
+  components: {
+    signUpForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
